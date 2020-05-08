a2b75872f15525d340328b9cb729ce5d03134666
Jakubrutkowski <jakubrutkowski95@gmail.com>
2020-05-08 15:17:25 +0200

    gitignore updated

--- Lab01/js/validator.js
+++ Lab01/js/validator.js
@@ -389,7 +389,7 @@ function sprawdzWszystko()
 
 
 
-        var row = '<tr><td class="nameInTable">' + table_name + '</td><td>' + table_code + '</td><td>' + table_netto + 'zł</td><td>' + table_brutto + 'zł</td><td>' + table_vat + '%</td><td>'+ table_category +'</td><td>' + table_options + '<td>' + table_rating + '</td><td>' + table_picture + '</td></tr>'
+        var row = '<tr><td class="nameInTable">' + table_name + '</td><td>' + table_code + '</td><td>' + table_netto + 'zł</td><td>' + table_brutto + 'zł</td><td>' + table_vat + '%</td><td>'+ table_category +'</td><td>' + table_options + '<td>' + table_rating + '</td><td>' + table_picture + '</td><td>' + '</td></tr>'
 
             $row = $(row),
             // resort table using the current sort; set to false to prevent resort, otherwise
